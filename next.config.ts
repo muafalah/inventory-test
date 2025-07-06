e29fe85950f15d6dc223e5932a525d90faf9119c
ifaamrillah <muh.ifa.amrillah@gmail.com>
2025-07-06 13:30:36 +0700

    feat: add next config image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["api.nanspace.top"],
+  },
 };
 
 export default nextConfig;
